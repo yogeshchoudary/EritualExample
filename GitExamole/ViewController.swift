79e3ec67b62336353b9af20ecf0bd840e6e7a142
Kumar Keshavamurthy <kumar.keshavamurthy@sakhatech.com>
2018-05-08 11:42:40 +0530

    Initial Commit

---
+++ GitExamole/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitExamole
+//
+//  Created by SakhaMacbook on 08/05/18.
+//  Copyright © 2018 SakhaMacbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
